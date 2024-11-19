4b69fcf47c87e5ca7450ce3a3ad43ae90a787dba
Simone Basso <bassosimone@gmail.com>
2024-11-19 10:57:11 +0100

    feat: start implementing cleartext dialing (#2)
    We're not quite there yet in terms of emitting events, but we have made
    some progress towards what we actually need.
    
    Loosely base the code on OONI Probe code for now, by cherry picking and
    going on memory while writing this.
    
    Also, document my current doubts regarding whether it would be better to
    have a unified type for dialing all connections types or whether
    separate types would be better. For now, keep the separate types and
    re-evaluate this in the future.

--- netcore/dialer.go
+++ netcore/dialer.go
@@ -1,9 +1,16 @@
+//
 // SPDX-License-Identifier: GPL-3.0-or-later
+//
+// Adapted from: https://github.com/ooni/probe-cli/blob/v3.20.1/internal/netxlite/dialer.go
+//
+// Cleartext conn dialer.
+//
 
 package netcore
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"net"
 )
@@ -25,6 +32,11 @@ type Dialer struct {
 	// structured diagnostic events. If this field is nil, we
 	// will not be emitting structured logs.
 	Logger *slog.Logger
+
+	// LookupHostFunc is the optional function to resolve a domain
+	// name to IP addresses. If this field is nil, we use the
+	// default [*net.Resolver] from the [net] package.
+	LookupHostFunc func(ctx context.Context, domain string) ([]string, error)
 }
 
 // NewDialer constructs a new [*Dialer] with default settings.
@@ -37,7 +49,71 @@ var DefaultDialer = NewDialer()
 
 // DialContext establishes a new TCP/UDP connection.
 func (d *Dialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
-	// TODO(bassosimone): decouple DNS lookup and dialing
+	// TODO(bassosimone): decide whether we want an overall timeout here
+
+	// resolve the domain name to IP addresses
+	domain, port, err := net.SplitHostPort(address)
+	if err != nil {
+		return nil, err
+	}
+	addrs, err := d.maybeLookupHost(ctx, domain)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO(bassosimone): decide whether we want to use happy eyeballs here
+
+	// attempt using each IP address
+	var errv []error
+	for _, addr := range addrs {
+		address = net.JoinHostPort(addr, port)
+		conn, err := d.dialLog(ctx, network, address)
+		if conn != nil && err == nil {
+			return conn, nil
+		}
+		errv = append(errv, err)
+	}
+	return nil, errors.Join(errv...)
+}
+
+// maybeLookupHost resolves a domain name to IP addresses unless the domain
+// is already an IP address, in which case we short circuit the lookup.
+func (d *Dialer) maybeLookupHost(ctx context.Context, domain string) ([]string, error) {
+	// handle the case where domain is already an IP address
+	if net.ParseIP(domain) != nil {
+		return []string{domain}, nil
+	}
+
+	// TODO(bassosimone): we should probably ensure we nonetheless
+	// include the lookup event inside the logs.
+	return d.doLookupHost(ctx, domain)
+}
+
+func (d *Dialer) doLookupHost(ctx context.Context, domain string) ([]string, error) {
+	// if there is a custom LookupHostFunc, use it
+	if d.LookupHostFunc != nil {
+		return d.LookupHostFunc(ctx, domain)
+	}
+
+	// otherwise fallback to the system resolver
+	reso := &net.Resolver{}
+	return reso.LookupHost(ctx, domain)
+}
+
+func (d *Dialer) dialLog(ctx context.Context, network, address string) (net.Conn, error) {
+	// TODO(bassosimone): emit structured logs
+	return d.dialNet(ctx, network, address)
+}
+
+func (d *Dialer) dialNet(ctx context.Context, network, address string) (net.Conn, error) {
+	// TODO(bassosimone): do we want to automatically wrap the connection?
+
+	// if there's an user provided dialer func, use it
+	if d.DialContextFunc != nil {
+		return d.DialContextFunc(ctx, network, address)
+	}
+
+	// otherwise use the net package
 	child := &net.Dialer{}
 	child.SetMultipathTCP(false)
 	return child.DialContext(ctx, network, address)
