6e3519abb03bdadf37d0e9c26bb3a0cee096f656
Simone Basso <bassosimone@gmail.com>
2024-11-19 17:04:10 +0100

    feat(netcore): implement observable TLS dialing (#6)
    With this diff, the code structure is ready to emit events.

--- netcore/tlsconfig.go
+++ netcore/tlsconfig.go
@@ -11,6 +11,16 @@ import (
 	"net"
 )
 
+// tlsConfig either returns the (cloned) [*tls.Config] from the [Network] or
+// creates a new one by invoking the [newTLSConfig] function.
+func (nx *Network) tlsConfig(network, address string) (*tls.Config, error) {
+	if nx.TLSConfig != nil {
+		config := nx.TLSConfig.Clone() // make sure we return a cloned config
+		return config, nil
+	}
+	return newTLSConfig(network, address)
+}
+
 // newTLSConfig is a best-effort attempt at creating a suitable TLS config
 // for TCP and UDP transports using the network and address.
 func newTLSConfig(network, address string) (*tls.Config, error) {
