0e28dad8769424391b86d92cc8e6b42e748f72bf
Simone Basso <bassosimone@gmail.com>
2024-11-30 11:50:31 +0100

    chore: deprecate the netipx package (#53)
    I moved it basically immediately to rbmk-project/common since x depends
    on dnscore, so dnscore cannot depend on x.
    
    While there, update deps.

--- netipx/netipx.go
+++ netipx/netipx.go
@@ -1,29 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 // Package netipx contains [net/netip] extensions.
+//
+// Deprecated: use github.com/rbmk-project/common/netipx instead.
 package netipx
 
-import (
-	"net"
-	"net/netip"
-)
+import "github.com/rbmk-project/common/netipx"
 
-// AddrToAddrPort converts a [net.Addr] to a [netip.AddrPort].
-//
-// If the input is nil or neither a [*net.TCPAddr] nor [*net.UDPAddr],
-// returns an unspecified IPv6 address with port 0.
-//
-// For [*net.TCPAddr] and [*net.UDPAddr] addresses, returns their
-// corresponding [netip.AddrPort] representation.
-func AddrToAddrPort(addr net.Addr) netip.AddrPort {
-	if addr == nil {
-		return netip.AddrPortFrom(netip.IPv6Unspecified(), 0)
-	}
-	if tcp, ok := addr.(*net.TCPAddr); ok {
-		return tcp.AddrPort()
-	}
-	if udp, ok := addr.(*net.UDPAddr); ok {
-		return udp.AddrPort()
-	}
-	return netip.AddrPortFrom(netip.IPv6Unspecified(), 0)
-}
+// AddrToAddrPort is an alias for [netipx.AddrToAddrPort].
+var AddrToAddrPort = netipx.AddrToAddrPort
